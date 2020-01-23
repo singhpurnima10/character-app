26d477e0d8ff6eb247aac65453ba20c5ed5dc9dc
Purnima SIngh <spurnima10aug@gmail.com>
2020-01-23 17:55:31 +0530

    data binding on UI

--- src/App.js
+++ src/App.js
@@ -1,59 +1,51 @@
 import React from "react";
 import { connect } from "react-redux";
-import { bindActionCreators } from 'redux'
+import { bindActionCreators } from "redux";
 import logo from "./logo.svg";
 import "./App.css";
-import { addUserAsync } from './redux/action'
+import { addUserAsync } from "./redux/action";
 import { render } from "react-dom";
+import * as _ from "lodash";
+import { StyledDiv,Image, MainContainer, Name } from "./App.style";
 
-class App extends React.Component{
-  componentDidMount(props){
+class App extends React.Component {
+  componentDidMount(props) {
     this.props.user1("value");
   }
-render(){
-  console.log(this.props,"dataaaaaaa")
-  return(
-    <div className="App">
-     <header className="App-header">
-  heyiiii
-  </header>
-      </div>
-  )
+  render() {
+    console.log(this.props.characterData, "dataaaaaaa");
+    const { characterData } = this.props;
+
+    return (
+      <MainContainer>
+        {_.map(characterData, (val, key) => {
+          return (
+            <div>
+              <StyledDiv>
+              <Image src={val.image}></Image>
+                <Name>NAME:{val.name}</Name>
+                <Name>STATUS:{val.status}</Name>
+                <Name>SPECIES:{val.species}</Name>
+                <Name>GENDER:{val.gender}</Name>
+                <Name>ORIGIN:{val.origin.name}</Name>
+                <Name>LAST LOCATION:{val.location.name}</Name>
+              </StyledDiv>
+            </div>
+          );
+        })}
+      </MainContainer>
+    );
+  }
 }
-}
-// {
-//   console.log(props,"propssssssssssss")
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//         <button onClick={() => props.user1('value')}>click me</button>
-//         {/* <div>{props.characterData}</div> */}
-//       </header>
-//     </div>
-//   );
-// }
 
 const mapStateToProps = state => {
-  return{
+  return {
     characterData: state && state.users
-  }
+  };
 };
 const mapDispatchToProps = dispatch => {
-  return{
-    user1: (value) => dispatch(addUserAsync(value))
-  }
-  
+  return {
+    user1: value => dispatch(addUserAsync(value))
+  };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/App.style.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+
+export const MainContainer = styled.div`
+padding: 40px;
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: center;
+`;
+
+
+export const StyledDiv = styled.div`
+  background-color: gray;
+  border: 12px solid black;
+  padding: 4px 10px;
+  color: white;
+  height:350px;
+  width:200px;
+  margin: 10px 50px 10px 20px;
+`
+
+export const Image = styled.img`
+height:40%;
+width:100%;
+`
+
+export const Name = styled.div`
+font-size:16px;
+color: orange;
+padding-top: 8px;`;
